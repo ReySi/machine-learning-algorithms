08fe475ed4bc4ba342479e39e1a7bbb746f31bee
Simon Rey <si_rey@web.de>
2017-03-03 14:30:58 +0100

    add decision_tree

---
+++ decision_tree.py
@@ -0,0 +1,27 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn import tree
+import os
+
+iris = load_iris()
+test_idx = [0,50,100]
+
+#training data
+train_target = np.delete(iris.target, test_idx)
+train_data = np.delete(iris.data, test_idx, axis=0)
+
+#testing data
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
+
+#classifier
+clf = tree.DecisionTreeClassifier()
+clf.fit(train_data, train_target)
+
+print test_target
+print clf.predict(test_data)
+
+with open("iris.dot", 'w') as f:
+    f = tree.export_graphviz(clf, out_file=f)
+
+os.unlink('iris.dot')
